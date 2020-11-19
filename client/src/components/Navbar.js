d980f50902cc2c442e084a3d2eb8ab00f9018652
Youness BENNAJ <youness.bennaj@gmail.com>
2020-11-19 19:11:40 +0100

    add login and signup component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -56,7 +56,7 @@ const Navbar = () => {
                                 <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
                                     <Link to="/profile" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Your Profile</Link>
 
-                                    {/* <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Settings</a> */}
+                                    <Link to="/authentication/login" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Login</Link>
 
                                     <a onClick={logout} href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Sign out</a>
                                 </div>
