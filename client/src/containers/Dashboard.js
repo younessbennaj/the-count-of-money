045913cc0448078f3f198dacb862d4e88a22ef8f
Youness BENNAJ <youness.bennaj@gmail.com>
2020-11-19 12:55:22 +0100

    add MSWJS to mock api data

---
+++ client/src/containers/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+const Dashboard = () => {
+
+    return (
+        <div>
+            <h1>Dashboard content here</h1>
+            <button>Click</button>
+        </div>
+    );
+}
+
+export default Dashboard;
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './assets/main.css'
 import App from './App';
 
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser')
+  worker.start()
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
