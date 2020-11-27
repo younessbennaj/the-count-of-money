a0f8400712838a332ed8cbc6425ab7705845ed21
Youness BENNAJ <youness.bennaj@gmail.com>
2020-11-27 11:52:20 +0100

    set up default config for axios and fetch data from the api

--- client/src/containers/Dashboard.js
+++ client/src/containers/Dashboard.js
@@ -27,15 +27,27 @@ const Dashboard = () => {
         //   })
 
         //With MSWJS actived
-        axios.get('/crypto')
-        .then(response => {
-            setData(response.data);
-        })
+
+        console.log(axios.defaults);
+
+        axios.get('/users/profile')
+            .then(response => {
+                console.log(response.data);
+            })
+
+        // axios.get('/users/profile')
+        //     .then(response => {
+        //         console.log(response.data);
+        //     })
+        // axios.get('http://localhost:5000/crypto')
+        //     .then(response => {
+        //         setData(response.data);
+        //     })
     }, []);
 
     function rowClick(e) {
         console.log(e);
-        if(e.rowType === "data") {
+        if (e.rowType === "data") {
             history.push({
                 pathname: '/cryptocurrency',
                 state: { cryptoId: e.component.getKeyByRowIndex(e.rowIndex).id } //this.props.location.state.detail
@@ -56,7 +68,7 @@ const Dashboard = () => {
                 onRowClick={rowClick}
             >
                 <Column dataField="id" width={0} />
-                <Column dataField="image" caption="" width={40} allowSorting={false} cellRender={ImageCell}/>
+                <Column dataField="image" caption="" width={40} allowSorting={false} cellRender={ImageCell} />
                 <Column dataField="name" width={100} />
                 <Column dataField="current_price" caption="Price" format="#0.####" dataType="number" />
                 {/* <Column dataField="quote.USD.price" caption="µChange" dataType="number" width={140} format="#0.####" cellRender={ChangeCell} /> */}
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from "axios";
+
+//Assets
 import './assets/main.css'
 import './style.css';
+
+//Components
 import App from './App';
 
+
+//If you want to work with the Mock API
 if (process.env.REACT_APP_API_CONFIG === 'development') {
   const { worker } = require('./mocks/browser')
   worker.start()
+} else {
+  console.log('real API');
+
+  //Instead, work with the real one
+
+  //We set the baseURL of our API
+  axios.defaults.baseURL = 'http://localhost:5000'
+
+  //We set the token in the request header
+  axios.defaults.headers.common['jwt'] = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZmMwZDg1OWVmZmIyMTNiMTQxODZlMGUiLCJpYXQiOjE2MDY0NzM4MzcsImV4cCI6MTYwNjU2MDIzN30.Ic7n6tTbD0ESbAXvKStZgxTwRH2kUS_9j6JpO04_aRQ";
 }
 
 ReactDOM.render(
